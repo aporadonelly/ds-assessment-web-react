366983434239d608d138a55ef08ea1ded418a0d4
Nelly Rose Aporado <aporadonelly@gmail.com>
2023-02-27 09:50:00 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+function App() {
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      minHeight="100vh"
+    >
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, mt: 0, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+        </Box>
+      </Container>
+    </Box>
+  );
+}
+
+export default App;
